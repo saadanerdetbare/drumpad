3e58c92cfb934f12ee99c9e2734f84b1dda84ed4
Jakob Dahl <jakobdahl@gmail.com>
2020-08-01 15:51:56 +0200

    update all functions to ES6

--- src/App.js
+++ src/App.js
@@ -4,43 +4,37 @@ import drums from 'drums';
 import 'App.scss';
 
 
-const SomethingWithGrid = () => {
-	return (
-		<Grid>
-			<Row>
-				<Col size={1}>
-					<p>noget i en Col</p>
-				</Col>
-			</Row>
-			<Row>
-				<Col size={2}>
-					noget andet i en col
-				</Col>
-				<Col size={1}>
-					ved siden af en col
-				</Col>
-			</Row>
-		</Grid>
-	)
-}
+const SomethingWithGrid = () => (
+	<Grid>
+		<Row>
+			<Col size={1}>
+				<p>noget i en Col</p>
+			</Col>
+		</Row>
+		<Row>
+			<Col size={2}>
+				noget andet i en col
+			</Col>
+			<Col size={1}>
+				ved siden af en col
+			</Col>
+		</Row>
+	</Grid>
+)
 
-const Footer = () => {
-	return (
-		<div className="footer">
-			<p>Made with React, much coffee and Stack<strong>overflow</strong></p>
-			<p>freeCodeCamp Drumpad project by Jakob Dahl</p>
-			<p>Code is on <a href="https://github.com/saadanerdetbare/drumpad" target="_blank" rel="noopener noreferrer">github</a></p>
-		</div>
-	)
-}
+const Footer = () => (
+	<div className="footer">
+		<p>Made with React, much coffee and Stack<strong>overflow</strong></p>
+		<p> <a href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine" target="_blank" rel="noopener noreferrer">freeCodeCamp Drumpad project</a> by Jakob Dahl</p>
+		<p>Code is on <a href="https://github.com/saadanerdetbare/drumpad" target="_blank" rel="noopener noreferrer">github</a></p>
+	</div>
+)
 
-function App() {
-	return (
-		<div>
-			<SomethingWithGrid />
-			<Footer />
-		</div>
-	);
-}
+const App = () => (
+	<>
+		<SomethingWithGrid />
+		<Footer />
+	</>
+)
 
 export default App;
