a3342ba5ea10069d6354062e2c4a4522acdfa6df
Jakob Dahl <jakobdahl@gmail.com>
2020-08-02 21:01:19 +0200

    added keyhandler package

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
-import React from 'react';
-import { Grid, Row, Col } from 'FlexGrid';
-import drums from 'drums';
-import 'App.scss';
+import React from "react";
+import { Grid, Row, Col } from "FlexGrid";
+import drums from "drums";
+import "App.scss";
 
-
-const SomethingWithGrid = () => (
-	<Grid>
-		<Row>
-			<Col size={1}>
-				<p>noget i en Col</p>
-			</Col>
-		</Row>
-		<Row>
-			<Col size={2}>
-				noget andet i en col
-			</Col>
-			<Col size={1}>
-				ved siden af en col
-			</Col>
-		</Row>
-	</Grid>
-)
+const SomethingWithGrid = (props) => (
+  <Grid>
+    <Row>
+      <Col size={1}>
+        <p>noget i en Col</p>
+      </Col>
+    </Row>
+    <Row>
+      <Col size={2}>noget andet i en col</Col>
+      <Col size={1}>ved siden af en col</Col>
+    </Row>
+  </Grid>
+);
 
 const Footer = () => (
-	<div className="footer">
-		<p>Made with React, much coffee and Stack<strong>overflow</strong></p>
-		<p> <a href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine" target="_blank" rel="noopener noreferrer">freeCodeCamp Drumpad project</a> by Jakob Dahl</p>
-		<p>Code is on <a href="https://github.com/saadanerdetbare/drumpad" target="_blank" rel="noopener noreferrer">github</a></p>
-	</div>
-)
+  <div className="footer">
+    <p>
+      Made with React, much coffee and Stack<strong>overflow</strong>
+    </p>
+    <p>
+      <a
+        href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        freeCodeCamp Drumpad project
+      </a>
+      by Jakob Dahl
+    </p>
+    <p>
+      Code is on
+      <a
+        href="https://github.com/saadanerdetbare/drumpad"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        github
+      </a>
+    </p>
+  </div>
+);
 
 const App = () => (
-	<>
-		<SomethingWithGrid />
-		<Footer />
-	</>
-)
+  <>
+    <SomethingWithGrid />
+    <Footer />
+  </>
+);
 
 export default App;
